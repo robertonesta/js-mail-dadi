a9a8789531a27cf24a0be6df34d074c1fd4dda11
RobertoNesta <robertonesta1993@gmail.com>
2023-03-13 14:50:55 +0100

    creating an array list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
+//creare lista di chi può accedere
+const userlist = [
+  'luca@gmail.com',
+  'mario@gmail.com',
+  'fabrizio@gmail.com',
+  'roberto@gmail.com',
+  'lorenzo@gmail.com'
+]
+
 
 //Chiedi all’utente la sua email
 //prompt
@@ -11,10 +20,8 @@ const button = document.querySelector("button");
 button.addEventListener ('click', function(){
     let inputmail = usermail.value;
     console.log(inputmail)
-
+    //controlla che sia nella lista di chi può accedere
 })
 
 
-
-//controlla che sia nella lista di chi può accedere
 //stampa un messaggio appropriato
