8788013cc26c6b8b1283249ac6f0ffbcfb9cd684
RobertoNesta <robertonesta1993@gmail.com>
2023-03-13 14:39:48 +0100

    creating a new input and button in html

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+//Chiedi all’utente la sua email
+//prompt
+
+const usermail = document.getElementById("mail");
+
+const button = document.querySelector("button");
+
+button.addEventListener ('click', function(){
+
+})
+
+
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio appropriato
